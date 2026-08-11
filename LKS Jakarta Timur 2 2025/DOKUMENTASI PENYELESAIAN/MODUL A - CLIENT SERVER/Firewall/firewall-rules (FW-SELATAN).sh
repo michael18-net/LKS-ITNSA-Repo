084170a2d4fd9d6f85c56d9caec9054023044138
Michael Davila <mike5509dvtobing1@gmail.com>
2026-08-11 12:20:53 +0700

    Add files via upload

---
+++ LKS Jakarta Timur 2 2025/DOKUMENTASI PENYELESAIAN/MODUL A - CLIENT SERVER/Firewall/firewall-rules (FW-SELATAN).sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Flush Old Rules
+iptables -F
+iptables -t nat -F
+iptables -X
+
+#Default Deny
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+#INPUT Rules
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+#FORWARD Rules
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i ens32 -s 172.16.20.0/24 -o ens33 -j ACCEPT
+iptables -A FORWARD -d 172.16.20.11 -p udp --dport 53 -j ACCEPT
+iptables -A FORWARD -d 172.16.20.11 -p tcp --dport 53 -j ACCEPT
+
+#NAT Prerouting
+iptables -t nat -A PREROUTING -i ens33 -d 103.10.70.120 -p udp --dport 53 -j DNAT --to-destination 172.16.20.11:53
+iptables -t nat -A PREROUTING -i ens33 -d 103.10.70.120 -p tcp --dport 53 -j DNAT --to-destination 172.16.20.11:53
+
+#NAT Postrouting
+iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
